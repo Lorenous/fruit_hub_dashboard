f1ddc9e5de655fe64db04c542b7118daa7523ba6
Lorenous <osamaabaelziz@gmail.com>
2026-04-18 13:42:34 +0200

    add project to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:fruit_hub_dashboard/core/helper_functions/on_generate_route.dart';
 import 'package:fruit_hub_dashboard/features/dashboard/presentation/views/dashboard_view.dart';
+import 'package:fruit_hub_dashboard/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
